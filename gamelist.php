9425fde7175ff149b3853062a9eddc04b98ef346
Nick <PS171871@edu.summacollege.nl>
2019-10-25 22:49:44 +0200

    JS Changes

---
+++ gamelist.php
@@ -0,0 +1,101 @@
+
+
+
+<head>
+<link rel="shortcut icon" type="image/png" href="http://localhost/projecten/wp-content/themes/shsite/css/images/36.png"/>
+</head>
+
+<body class="home page-template page-template-page-home page-template-page-home-php page page-id-257">
+<header>
+<div class="container">
+<nav class="navbar navbar-default">
+<div class="navbar-header">
+<div class="pull-left">
+</div>
+<a class="navbar-brand" href="#">
+<img src="http://localhost/projecten/wp-content/themes/shsite/css/images/fg.png" class="logo-big" width="150" height="150" alt="Logo">
+<img src="http://localhost/projecten/wp-content/themes/shsite/css/images/fg-small.png" class="logo-small" width="160" height="55" alt="Logo">
+</a>
+<div class="pull-right">
+<button class="hamburger hamburger-animation" data-target="#menu">
+<span class="sr-only">Toggle navigation</span>
+<span>Menu</span>
+</button>
+</div>
+</div>
+<div id="menu" class="navbar-collapse">
+<ul id="menu-menu" class="nav navbar-nav"><li id="menu-item-260" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-257 current_page_item menu-item-260"><a href="http://localhost/projecten/">Home</a></li>
+<li id="menu-item-261" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-261"><a href="http://localhost/projecten/nieuwsbrief/">Nieuwsbrief</a></li>
+<li id="menu-item-258" class="btn outline menu-item menu-item-type-post_type menu-item-object-page menu-item-258"><a href="http://localhost/projecten/contact/">Contact</a></li>
+</ul></div>
+</nav>
+</div>
+</header>
+
+<div class="content">
+<div class="banner">
+<div class="banner-content">
+<div class="container">
+<div class="title">Nieuwsbrief</div>
+<div class="subtitle vidaloka">De nieuwsbrief van NC Tuinservice</div>
+<div class="btn-group">
+<a href="http://localhost/projecten" class="btn btn-primary">Home</a>
+<a href="http://localhost/projecten/contact/" class="btn btn-darkgreen">Contact</a>
+</div>
+<i class="fa fa-chevron-down" aria-hidden="true"></i>
+</div>
+</div>
+<div class="banner-image">
+<img src="http://localhost/projecten/wp-content/themes/shsite/css/images/soma2.jpg" alt="">
+</div>
+</div>
+
+
+
+
+<section class="over text-center">
+        <div class="container-fluid">
+            <div class="row vertical-align">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-grey">
+                    <div class="fifty-wide">
+                        <div class="titel nopadding"><em class="vidaloka">Nieuwsartikel nummer 1</em></div>
+                        <div class="seperator"></div>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+                        <a href="#" class="btn btn-outline">Lees meer</a>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    <img src="http://localhost/projecten/wp-content/themes/shsite/css/images/sp/soma1.jpg" class="img-responsive"></div>
+            </div></div>
+    </section>
+
+    <section class="over text-center">
+        <div class="container-fluid">
+            <div class="row vertical-align">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    <img src="http://localhost/projecten/wp-content/themes/shsite/css/images/sp/plant.jpg" class="img-responsive">
+                </div>
+
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-grey">
+                    <div class="fifty-wide">
+                        <div class="titel nopadding"><em class="vidaloka">Vliegende plant</em></div>
+                        <div class="seperator"></div>
+                        <p>Wat een mooie vliegende plant, erg apart. Maar wel mooi. </p>
+                        <a href="#" class="btn btn-outline">Lees meer</a>
+                    </div>
+                </div>
+            </div>
+            
+            </div>
+    </section>
+
+
+
+
+<link rel="dns-prefetch" href="//maxcdn.bootstrapcdn.com"/>
+<link rel="stylesheet" href="http://localhost/projecten/wp-content/themes/shsite/css/front-min.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" id="plugin-css" href="http://localhost/projecten/wp-content/themes/shsite/css/plugin.css" type="text/css" media="all"/>
+<link rel="stylesheet" id="theme-css" href="http://localhost/projecten/wp-content/themes/shsite/css/theme.css" type="text/css" media="all"/>
+<link rel="stylesheet" id="responsive-css" href="http://localhost/projecten/wp-content/themes/shsite/css/responsive.css" type="text/css" media="all"/>
+<link rel="stylesheet" id="fontawesome-css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" media="all"/>
+<?php include 'footer.php';?>
